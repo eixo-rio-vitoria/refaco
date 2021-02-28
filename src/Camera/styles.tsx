e7e8137e877c65e9618cc14176a0bbb1fda086fd
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2021-02-28 09:51:55 -0300

    feat: style modal-container

--- src/Camera/styles.tsx
+++ src/Camera/styles.tsx
@@ -5,11 +5,14 @@ export const Container = styled.View`
 `;
 
 export const ModalContainer = styled.View`
-	flex: 1;
+	flex: 0.7;
 	background-color: white;
+	align-items: center;
+	border-radius: 12px;
 `;
 
 export const ModalTitle = styled.Text`
+	margin-top: 20px;
 	font-size: 20px;
 	color: black;
 	font-weight: bold;
