06d7cb6a04f6b596522ded10ea530a870f507b6e
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2021-02-28 09:40:10 -0300

    feat: add modal title component

--- src/Camera/styles.tsx
+++ src/Camera/styles.tsx
@@ -3,3 +3,14 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
 	flex: 1;
 `;
+
+export const ModalContainer = styled.View`
+	flex: 1;
+	background-color: white;
+`;
+
+export const ModalTitle = styled.Text`
+	font-size: 20px;
+	color: black;
+	font-weight: bold;
+`;
